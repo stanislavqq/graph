fb4186b6aea711fc278da656124a1737acedc183
bretgauer <bretgauer@obit.ru>
2023-05-24 15:04:41 +0300

    Updated app

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"graph/app"
 	"graph/config"
 	"graph/parser"
 	"graph/server"
 	"net/http"
 	"os"
+	"os/signal"
 )
 
 var logger zerolog.Logger
 
 func init() {
-	logger = zerolog.Logger{}
+	logger = log.Logger
 	err := godotenv.Load()
 	if err != nil {
 		logger.Fatal().Err(err).Msg("Error loading .env file")
@@ -33,19 +38,38 @@ func main() {
 	appChart := app.NewApp(MChart)
 
 	cfg := config.Config{}
-	cfg.HttpConfig = config.HttpConfig{
-		Host: "127.0.0.1",
-		Port: "8000",
-	}
+	cfg.HttpConfig = config.HttpConfig{Host: "", Port: ""}
 
+	host := flag.String("host", "127.0.0.1", "-host 127.0.0.1")
+	port := flag.String("port", "8000", "-port 8000")
+	flag.Parse()
+
+	cfg.HttpConfig.Host = *host
+	cfg.HttpConfig.Port = *port
+
+	fmt.Println(cfg.HttpConfig.Host)
+
+	ctx := context.Background()
 	srv := server.New(cfg.HttpConfig, logger)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt)
 
 	srv.Router.HandleFunc("/getChart", appChart.GetChart).Methods("GET")
 	srv.Router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./index.html")
 	}).Methods("GET")
 
-	if err = srv.Start(); err != nil {
+	go func() {
+		select {
+		case <-done:
+			logger.Info().Msg("Get exit signal")
+			logger.Info().Msg("Http server close")
+			defer srv.Stop()
+			//os.Exit(1)
+		}
+	}()
+
+	if err = srv.Start(ctx); err != nil {
 		logger.Fatal().Err(err)
 		return
 	}
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
@@ -13,6 +14,7 @@ type Server struct {
 	Router     *mux.Router
 	Logger     zerolog.Logger
 	HttpConfig config.HttpConfig
+	HttpServer *http.Server
 }
 
 func New(cfg config.HttpConfig, logger zerolog.Logger) *Server {
@@ -28,18 +30,27 @@ func New(cfg config.HttpConfig, logger zerolog.Logger) *Server {
 	return srv
 }
 
-func (s *Server) Start() error {
-	srv := &http.Server{
+func (s *Server) Start(ctx context.Context) error {
+	s.HttpServer = &http.Server{
 		Handler:      s.Router,
 		Addr:         fmt.Sprintf("%s:%s", s.HttpConfig.Host, s.HttpConfig.Port),
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
+	s.Logger.Info().Msg(fmt.Sprintf("Http Server started on %s:%s", s.HttpConfig.Host, s.HttpConfig.Port))
+
+	if err := s.HttpServer.ListenAndServe(); err != nil {
 		s.Logger.Fatal().Err(err)
 		return err
 	}
 
 	return nil
+}
+
+func (s *Server) Stop() {
+	if err := s.HttpServer.Close(); err != nil {
+		s.Logger.Error().Err(err)
+		return
+	}
 }
